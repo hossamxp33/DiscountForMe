82261e568f22429699b30842e4fb541b3b7430fd
Ibrahim Alshehidy <ibrahim.alshehidy@gmail.com>
2020-11-25 01:44:59 +0200

    language.

---
+++ app/src/main/java/com/headshot/discountforme/SplashAndIntro/ChooseLanguage/View/ChooseLanguageActivity.java
@@ -0,0 +1,15 @@
+package com.headshot.discountforme.SplashAndIntro.ChooseLanguage.View;
+
+import android.os.Bundle;
+
+import com.headshot.discountforme.R;
+import com.headshot.discountforme.Utils.ParentClass;
+
+public class ChooseLanguageActivity extends ParentClass {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_language);
+    }
+}
