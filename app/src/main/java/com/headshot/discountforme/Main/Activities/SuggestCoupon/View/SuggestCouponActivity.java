62d3b5c06119595c36de2b781c5adb392ef4ea48
Ibrahim Alshehidy <ibrahim.alshehidy@gmail.com>
2020-11-26 01:43:54 +0200

    complete design

---
+++ app/src/main/java/com/headshot/discountforme/Main/Activities/SuggestCoupon/View/SuggestCouponActivity.java
@@ -0,0 +1,15 @@
+package com.headshot.discountforme.Main.Activities.SuggestCoupon.View;
+
+import android.os.Bundle;
+
+import com.headshot.discountforme.R;
+import com.headshot.discountforme.Utils.ParentClass;
+
+public class SuggestCouponActivity extends ParentClass {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggest_coupon);
+    }
+}
