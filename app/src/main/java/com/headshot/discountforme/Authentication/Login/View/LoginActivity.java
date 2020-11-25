3f1348bc5b4bb08ec76a712bda1c7389536fda90
Ibrahim Alshehidy <ibrahim.alshehidy@gmail.com>
2020-11-25 03:40:13 +0200

    language.

---
+++ app/src/main/java/com/headshot/discountforme/Authentication/Login/View/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.headshot.discountforme.Authentication.Login.View;
+
+import android.os.Bundle;
+
+import com.headshot.discountforme.R;
+import com.headshot.discountforme.Utils.ParentClass;
+
+public class LoginActivity extends ParentClass {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
